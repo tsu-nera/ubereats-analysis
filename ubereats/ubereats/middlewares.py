6d462aa48f3353679660cf008394015fe8111c36
tsu-nera <fox10225fox@gmail.com>
2019-12-22 10:20:25 +0900

    Scrapyのプロジェクト名前変更

--- ubereats/ubereats/middlewares.py
+++ ubereats/ubereats/middlewares.py
@@ -8,7 +8,7 @@
 from scrapy import signals
 
 
-class WebscrapingSpiderMiddleware(object):
+class UbereatsSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +56,7 @@ class WebscrapingSpiderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class WebscrapingDownloaderMiddleware(object):
+class UbereatsDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
