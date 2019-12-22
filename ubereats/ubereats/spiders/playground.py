5cbffe892cdfd2db53381402335112b19a5cce4f
tsu-nera <fox10225fox@gmail.com>
2019-12-22 14:28:35 +0900

    wip

--- ubereats/ubereats/spiders/playground.py
+++ ubereats/ubereats/spiders/playground.py
@@ -2,14 +2,19 @@
 import scrapy
 from ..items import ShopItem
 
+UBEREATS_URL = "https://www.ubereats.com/ja-JP/"
+UBEREATS_KAWASAKI_URL = "https://www.ubereats.com/ja-JP/kawasaki/"
+UBEREATS_SEARCH_MUSASHINAKAHARA_URL = "https://www.ubereats.com/ja-JP/feed/?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMiVFNiVBRCVBNiVFOCU5NCVCNSVFNCVCOCVBRCVFNSU4RSU5RiVFOSVBNyU4NSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpkYXpMbUtMMUdHQVJHSjFYRzlJZVpWWSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0EzNS41ODA3MTQzJTJDJTIybG9uZ2l0dWRlJTIyJTNBMTM5LjY0MjEwNyU3RA%3D%3D/"
+
 
 class PlaygroundSpider(scrapy.Spider):
     name = 'playground'
     allowed_domains = ['www.ubereats.com']
-    start_urls = ['https://www.ubereats.com/ja-JP/kawasaki/']
+
+    start_urls = [UBEREATS_SEARCH_MUSASHINAKAHARA_URL]
 
     def parse(self, response):
         shop = ShopItem()
-        shop["name"] = response.css("article div::text")[1].extract()
+        shop["name"] = response.css("body").extract()
 
         yield shop
