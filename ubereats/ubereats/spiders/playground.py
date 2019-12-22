99dd3f9b216b5e6c9797b55527410a8862844e64
tsu-nera <fox10225fox@gmail.com>
2019-12-22 13:41:49 +0900

    デニーズ高津店が取得できた

--- ubereats/ubereats/spiders/playground.py
+++ ubereats/ubereats/spiders/playground.py
@@ -6,10 +6,10 @@ from ..items import ShopItem
 class PlaygroundSpider(scrapy.Spider):
     name = 'playground'
     allowed_domains = ['www.ubereats.com']
-    start_urls = ['https://www.ubereats.com/ja-JP/']
+    start_urls = ['https://www.ubereats.com/ja-JP/kawasaki/']
 
     def parse(self, response):
         shop = ShopItem()
-        shop["name"] = response.css('a.b4.b5.bq.b7::text').extract()
+        shop["name"] = response.css("article div::text")[1].extract()
 
         yield shop
