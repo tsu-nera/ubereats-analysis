ce9284262c991f7da2ef0a8459b807ac12d4cea7
tsu-nera <fox10225fox@gmail.com>
2019-12-23 19:29:44 +0900

    もどした

--- tasks.py
+++ tasks.py
@@ -1,13 +1,11 @@
 import invoke
-from datetime import datetime
 
 
 @invoke.task
 def crawl(c):
     data_dir = "rawdata"
-    now = datetime.datetime.now()
-    data_file = now.strftime('%Y%m%d_%H%M%S') + "_musashinakahara.csv"
+    data_file = "musashinakahara.csv"
     data_path = data_dir + "/" + data_file
 
-    command = "cd ubereats && scrapy crawl feed -o ../" + data_path  # noqa
+    command = 'cd ubereats && rm ../' + data_path + " && scrapy crawl feed -o ../" + data_path  # noqa
     invoke.run(command)
