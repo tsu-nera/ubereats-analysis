99330bfa5281548b8906924afc2ee88a085a7eaa
tsu-nera <fox10225fox@gmail.com>
2019-12-23 19:28:39 +0900

    update

--- tasks.py
+++ tasks.py
@@ -9,5 +9,5 @@ def crawl(c):
     data_file = now.strftime('%Y%m%d_%H%M%S') + "_musashinakahara.csv"
     data_path = data_dir + "/" + data_file
 
-    command = 'cd ubereats && rm ../' + data_path + " && scrapy crawl feed -o ../" + data_path  # noqa
+    command = "cd ubereats && scrapy crawl feed -o ../" + data_path  # noqa
     invoke.run(command)
