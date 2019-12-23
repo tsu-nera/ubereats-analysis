9d04f6c4abe7ca789747f617da81a291bc646412
tsu-nera <fox10225fox@gmail.com>
2019-12-23 19:27:46 +0900

    filename改良

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
 import invoke
+from datetime import datetime
 
 
 @invoke.task
 def crawl(c):
     data_dir = "rawdata"
-    data_file = "musashinakahara.csv"
+    now = datetime.datetime.now()
+    data_file = now.strftime('%Y%m%d_%H%M%S') + "_musashinakahara.csv"
     data_path = data_dir + "/" + data_file
 
     command = 'cd ubereats && rm ../' + data_path + " && scrapy crawl feed -o ../" + data_path  # noqa
