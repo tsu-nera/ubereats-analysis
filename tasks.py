8e79a8f6a06d1ce1d5388b3ddf391dea73507e7e
tsu-nera <fox10225fox@gmail.com>
2019-12-23 23:47:14 +0900

    update

--- tasks.py
+++ tasks.py
@@ -6,7 +6,8 @@ from datetime import datetime
 def crawl(c):
     data_dir = "rawdata"
     now = datetime.now()
-    data_file = now.strftime('%y%m%d_%H%M%S') + "_musashinakahara.csv"
+    # data_file = now.strftime('%y%m%d_%H%M%S') + "_musashinakahara.csv"
+    data_file = now.strftime('%y%m%d_%H%M%S') + "_musashikosugi_search.csv"
     data_path = data_dir + "/" + data_file
 
     command = "cd ubereats && scrapy crawl feed -o ../" + data_path  # noqa
