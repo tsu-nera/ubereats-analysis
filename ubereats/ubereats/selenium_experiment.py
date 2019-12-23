24721c187f5c25ea1be62de86a59590a72f59955
tsu-nera <fox10225fox@gmail.com>
2019-12-23 13:05:26 +0900

    done

--- ubereats/ubereats/selenium_experiment.py
+++ ubereats/ubereats/selenium_experiment.py
@@ -3,9 +3,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 
 options = ChromeOptions()
 
-# Chromeを閉じて--remote-debugging-portを指定するか、headlessをつけるのどちらか
 # options.add_argument("--headless")
-# options.add_argument("--remote-debugging-port=9222")
 
 options.add_argument("start-maximized")
 options.add_argument("--no-sandbox")
@@ -18,7 +16,7 @@ driver = Chrome(options=options)
 
 driver.get('https://www.google.com/')
 time.sleep(5)
-[]
+
 search_box = driver.find_element_by_name("q")
 search_box.send_keys('ChromeDriver')
 search_box.submit()
--- ubereats/ubereats/spiders/feed.py
+++ ubereats/ubereats/spiders/feed.py
@@ -1,5 +1,10 @@
 import scrapy
 
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 from ..items import ShopItem
 from ..constants import MUSASHINAKAHARA_FEED_URL, BASE_DOMAIN, BASE_URL  # noqa
 
@@ -9,14 +14,36 @@ class FeedSpider(scrapy.Spider):
     allowed_domains = [BASE_DOMAIN]
     start_urls = [MUSASHINAKAHARA_FEED_URL]
 
+    def __init__(self):
+        options = ChromeOptions()
+
+        # options.add_argument("--headless")
+        options.add_argument("start-maximized")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+
+        self.driver = Chrome(options=options)
+
     def parse(self, response):
         print("Parsing...\n")
 
-        for href in response.xpath("//a/@href").re(
-                '(/ja-JP/.*/food-delivery/.*)'):
+        self.driver.get(response.url)
+
+        WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "article.af")))
+
+        res = response.replace(
+            body=self.driver.page_source)  # レスポンスオブジェクトのHTMLをseleniumのものと差し替える
+
+        for href in res.xpath("//a/@href").re('(/ja-JP/.*/food-delivery/.*)'):
             full_url = BASE_URL + href
 
             yield scrapy.Request(full_url, callback=self.parse_item)
+
+        self.driver.close()
 
     def parse_item(self, response):
 
