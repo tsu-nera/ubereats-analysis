b53f66ea5b213e4b352f8d4fe1d41ed41b3e183c
tsu-nera <fox10225fox@gmail.com>
2019-12-27 17:01:51 +0900

    tripitemの追加

--- ubereats/ubereats/items/shop.py
+++ ubereats/ubereats/items/shop.py
@@ -2,7 +2,7 @@ import scrapy
 
 
 class ShopItem(scrapy.Item):
-    id = scrapy.Field()  # id
+    id = scrapy.Field()  # Id
     name = scrapy.Field()  # 店名
     reviews = scrapy.Field()  # レビュー数
     point = scrapy.Field()  # 5点評価
