a69f1bb4dc540eb4e896416ae28f42f8620dc45d
tsu-nera <fox10225fox@gmail.com>
2019-12-30 13:18:27 +0900

    add merge script

---
+++ merge_trip.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+MASTER_FILE_PATH = './data/trip_master.csv'
+TARGET_FILE_PATH = "./rawdata/trips/191230_130421_trips.csv"
+
+master = pd.read_csv(MASTER_FILE_PATH, index_col='id')
+df = pd.read_csv(TARGET_FILE_PATH, index_col="id")
+
+data = pd.concat([master, df], sort=True).drop_duplicates(subset="url")
+
+data.to_csv(MASTER_FILE_PATH, index=True)
