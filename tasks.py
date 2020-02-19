44a464e742800e4fe60b7e9b455a4c6eecf367ad
tsu-nera <fox10225fox@gmail.com>
2020-02-19 13:27:37 +0900

    wip

--- tasks.py
+++ tasks.py
@@ -97,7 +97,7 @@ def trip(c, year, month, day):
 
     if os.path.exists(data_path):
         os.remove(data_path)
-    command = "cd ubereats && scrapy crawl -a year={} -a month={} -a day={} trip -o ../{}".format(  # noqa
+    command = "cd ubereats && scrapy crawl -a year={} -a month={} -a day={} trip -o ../{} -t 'csv'".format(  # noqa
         data_path, year, month, day, data_path)  # noqa
     invoke.run(command)
 
